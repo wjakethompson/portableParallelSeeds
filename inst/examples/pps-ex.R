a2fbe96885471842fe50dbce07626f984f95915d
Paul E. Johnson <pauljohn@ku.edu>
2015-11-02 12:32:26 -0600

    clarify dialogue
    pps-ex.R needed human-readable instructions

--- inst/examples/pps-ex.R
+++ inst/examples/pps-ex.R
@@ -37,36 +37,47 @@ runOneSimulation <- function(run, streamsource, N, m, sd){
 
 \donttest{
 ## Give seed collection object to each simulation, let each pick desired seed
-serial1 <- lapply(1:1000, runOneSimulation, projSeeds, N=800, m = 14, sd = 10.1)
+    serial1 <- lapply(1:1000, runOneSimulation, projSeeds, N=800, m = 14, sd = 10.1)
+    
+    
+    ## First re-load the seed object, then give to simulations
+    fruits2 <- readRDS("fruits.rds")
+    serial2 <- lapply(1:1000, runOneSimulation, fruits2, N=800, m = 14, sd = 10.1)
+    
+    
+    ## Re-load file separately in each run
+    serial3 <- lapply(1:1000, runOneSimulation, "fruits.rds", N = 800, m = 14, sd = 10.1)
+    identical(serial1, serial2)
+    identical(serial1, serial3)
+    ## The results form the 3 lapply statements are all identical.
 
+    ## Lets check run 912, you'll see what I mean.
+    serial1[[912]]
+    serial2[[912]]
+    
+    
+    ## Next question. Can we re-set the current interactive session's seed
+    ## and re-draw that same value?
 
-## First re-load the seed object, then give to simulations
-fruits2 <- readRDS("fruits.rds")
-serial2 <- lapply(1:1000, runOneSimulation, fruits2, N=800, m = 14, sd = 10.1)
+    ## Put the seeds from run 912 back into the current session
+    setSeeds("fruits.rds", run = 912, verbose = FALSE)
 
-
-## Re-load file separately in each run
-serial3 <- lapply(1:1000, runOneSimulation, "fruits.rds", N = 800, m = 14, sd = 10.1)
-identical(serial1, serial2)
-identical(serial1, serial3)
-
-## Next question. Is the 801'th random normal from stream 1 equal to
-## the 3'rd element in the returned vector? Lets check run 912
-
-setSeeds("fruits.rds", run = 912, verbose = FALSE)
-(X801 <- rnorm(801, m=14, sd = 10.1)[801])
-
-## Look at the 801'th element of X801
-
-X801
-
-## Compare with the 3rd element in serial1's 912th element
-serial1[[912]]
-
-##Bingo. I'm right. Can draw a understandably replicatable streams of
-## random numbers, whether we draw 800, switch to a different stream,
-## and then change back to draw another, or if we just draw 801 in one
-## series.
-
-unlink("fruits.rds") #delete file
+    ## Draw 801 elements    
+    X <- rnorm(801, m=14, sd = 10.1)
+    ## The 801'th element should be same as saved in the third element
+    ## of the serial1[[912]] object.
+    X[801]
+    
+    if(!all.equal(unname(X[801] - serial1[[912]][3]), 0)){
+        stop("The difference is not 0, something went wrong")
+    } else {
+        print("Celebrate. The value in X[801] equals the serial1[[912]][3] value")
+    }
+    
+    ##Bingo. I'm right. Can draw a understandably replicatable streams of
+    ## random numbers, whether we draw 800, switch to a different stream,
+    ## and then change back to draw another, or if we just draw 801 in one
+    ## series.
+    
+    unlink("fruits.rds") #delete file
 }
